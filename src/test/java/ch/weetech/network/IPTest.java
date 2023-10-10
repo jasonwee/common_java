86b024e5454820b0260b3de0b0ef3a71beb5f0d5
Jason Wee <peichieh@gmail.com>
2023-10-10 09:00:49 +0800

    more unit tests

--- src/test/java/ch/weetech/network/IPTest.java
+++ src/test/java/ch/weetech/network/IPTest.java
@@ -26,14 +26,26 @@ public class IPTest {
     @Test
     public void testIPV4() {
 
-        assertTrue(IP.isIpInSubnet("192.168.0.3", "192.168.0.0", "24"));
+        assertTrue(IP.isIpInSubnet("192.168.0.3", "192.168.0.0", "255.255.255.0"));
+        assertTrue(IP.isIpInSubnet("192.168.2.1", "192.168.2.1", "255.255.255.255"));
+        assertTrue(IP.isIpInSubnet("192.168.2.5", "192.168.2.0", "255.255.255.0"));
+        assertTrue(IP.isIpInSubnet("10.10.20.3", "10.10.20.0", "255.255.255.252"));
+        assertTrue(IP.isIpInSubnet("192.200.0.0", "10.10.20.0", "255.255.255.252"));
 
+        assertFalse(IP.isIpInSubnet("192.168.2.1", "192.168.2.0", "255.255.255.255"));
+        assertFalse(IP.isIpInSubnet("10.10.20.5", "10.10.20.0", "255.255.255.252"));
+        assertFalse(IP.isIpInSubnet("10.10.0.1", "192.168.0.0", "255.255.255.0"));
 
     }
 
     @Test
     public void testIPV6() {
         assertTrue(IP.isIpInSubnet("fe80:0:0:0:0:0:c0a8:11", "fe80:0:0:0:0:0:c0a8:1", 120));
+        assertTrue(IP.isIpInSubnet("1::1", "1::", 64));
+        assertTrue(IP.isIpInSubnet("2001:db8::4", "2001:db8::", 32));
+
+        assertFalse(IP.isIpInSubnet("fe80:0:0:0:0:0:c0a8:11", "fe80:0:0:0:0:0:c0a8:1", 128));
+        assertFalse(IP.isIpInSubnet("2::1", "1::", 64));
     }
 
 }
