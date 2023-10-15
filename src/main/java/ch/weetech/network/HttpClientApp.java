9d0158e527231726deae4986dde51e0356c0135a
Jason Wee <peichieh@gmail.com>
2023-10-15 19:08:50 +0800

    implement http post

--- src/main/java/ch/weetech/network/HttpClientApp.java
+++ src/main/java/ch/weetech/network/HttpClientApp.java
@@ -26,6 +26,8 @@ import java.net.http.HttpConnectTimeoutException;
 import java.net.http.HttpRequest;
 import java.net.http.HttpTimeoutException;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpClientApp {
 
@@ -50,13 +52,12 @@ public class HttpClientApp {
                 .setHeader("User-Agent", userAgent)
                 .build();
 
-        java.net.http.HttpResponse<String> response;
         HttpResponse httpResponse = new HttpResponse();
         int tries = retriesCount;
 
         do {
             try {
-                response = httpClient.send(request, java.net.http.HttpResponse.BodyHandlers.ofString());
+                java.net.http.HttpResponse<String> response = httpClient.send(request, java.net.http.HttpResponse.BodyHandlers.ofString());
                 httpResponse.setStatusCode(response.statusCode());
                 httpResponse.setContent(response.body());
                 break;
@@ -91,11 +92,65 @@ public class HttpClientApp {
         return get(url, "HttpClientApp", 5, 0);
     }
 
-    // TODO IMPLEMENT ME
-    public HttpResponse post(String url) {
-        return null;
+    public HttpResponse post(String url, Map<Object, Object> data, Map<String, String> headers, String userAgent, int readTimeout, int retriesCount) {
+
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder();
+        requestBuilder.uri(URI.create(url));
+        requestBuilder.timeout(Duration.ofSeconds(readTimeout));
+        requestBuilder.setHeader("User-Agent", userAgent);
+
+        for (var entry : headers.entrySet()) {
+            requestBuilder.header(entry.getKey(), entry.getValue());
+        }
+
+        if (data != null) {
+            requestBuilder.POST(HttpUtil.buildFormDataFromMap(data));
+        } else {
+            requestBuilder.POST(HttpRequest.BodyPublishers.noBody());
+        }
+
+        HttpRequest request = requestBuilder.build();
+        HttpResponse httpResponse = new HttpResponse();
+        int tries = retriesCount;
+
+        do {
+            java.net.http.HttpResponse<String> response;
+            try {
+                response = httpClient.send(request, java.net.http.HttpResponse.BodyHandlers.ofString());
+                httpResponse.setStatusCode(response.statusCode());
+                httpResponse.setContent(response.body());
+                break;
+            } catch (HttpConnectTimeoutException e) {
+                httpResponse.setStatusCode(HttpResponseCode.CLIENT_CONNECT_TIMEOUT);
+                StringWriter sw = new StringWriter();
+                e.printStackTrace(new PrintWriter(sw));
+                httpResponse.setError(sw.toString());
+            } catch (HttpTimeoutException e) {
+                httpResponse.setStatusCode(HttpResponseCode.CLIENT_READ_TIMEOUT);
+                StringWriter sw = new StringWriter();
+                e.printStackTrace(new PrintWriter(sw));
+                httpResponse.setError(sw.toString());
+            } catch (IOException e) {
+                httpResponse.setStatusCode(HttpResponseCode.CLIENT_IO_ERROR);
+                StringWriter sw = new StringWriter();
+                e.printStackTrace(new PrintWriter(sw));
+                httpResponse.setError(sw.toString());
+            } catch (InterruptedException e) {
+                httpResponse.setStatusCode(HttpResponseCode.CLIENT_INTERRUPTED_EXCEPTION);
+                StringWriter sw = new StringWriter();
+                e.printStackTrace(new PrintWriter(sw));
+                httpResponse.setError(sw.toString());
+            }
+            tries--;
+        } while (tries > 0);
+
+        return httpResponse;
     }
 
-
+        public HttpResponse post(String url, Map<Object, Object> data) {
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Content-Type", "application/x-www-form-urlencoded");
+            return post(url, data, headers, "HttpClientApp", 5, 0);
+        }
 
 }
