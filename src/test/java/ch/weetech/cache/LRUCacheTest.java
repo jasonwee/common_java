5dc9d1da673c761c1ecf7eaeb8dcc554313f47b0
Jason Wee <peichieh@gmail.com>
2023-10-18 20:20:47 +0800

    lrucache unit test

---
+++ src/test/java/ch/weetech/cache/LRUCacheTest.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.weetech.cache;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+
+public class LRUCacheTest {
+
+    @Test
+    public void intro() {
+        LRUCache<String, String> lruCache = new LRUCache<>(3);
+        lruCache.put("1", "test1");
+        lruCache.put("2", "test2");
+        lruCache.put("3", "test3");
+        assertEquals("test1", lruCache.get("1").get());
+        assertEquals("test2", lruCache.get("2").get());
+        assertEquals("test3", lruCache.get("3").get());
+    }
+
+    @Test
+    public void testEvince() {
+        LRUCache<String, String> lruCache = new LRUCache<>(3);
+        lruCache.put("1", "test1");
+        lruCache.put("2", "test2");
+        lruCache.put("3", "test3");
+        lruCache.put("4", "test4");
+        assertFalse(lruCache.get("1").isPresent());
+    }
+
+    @Test
+    public void testConcurrent() throws Exception {
+        final int size = 50;
+        final ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Cache<Integer, String> cache = new LRUCache<>(size);
+        CountDownLatch countDownLatch = new CountDownLatch(size);
+        try {
+            IntStream.range(0, size).<Runnable>mapToObj(key -> () -> {
+                cache.put(key, "value" + key);
+                countDownLatch.countDown();
+            }).forEach(executorService::submit);
+            countDownLatch.await();
+        } finally {
+            executorService.shutdown();
+        }
+        assertEquals(cache.size(), size);
+        IntStream.range(0, size).forEach(i -> assertEquals("value" + i, cache.get(i).get()));
+    }
+
+}
